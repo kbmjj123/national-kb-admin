11e5c1656f6d4b2e8b5e069c452312274d07f1d6
郑耿林 <kbmjj123@gmail.com>
2024-02-16 08:26:40 +0800

    ✨ feat(登录页面): 完成登录页面的开发

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,158 @@
 <template>
-  <n-card>
-    我是登录页面
-  </n-card>
+  <div class="view-account">
+    <div class="view-account-header"></div>
+    <div class="view-account-container">
+      <div class="view-account-top">
+        <div class="view-account-top-logo">
+          <img :src="websiteConfig.loginImage" alt="" />
+        </div>
+        <div class="view-account-top-desc">{{ websiteConfig.loginDesc }}</div>
+      </div>
+      <div class="view-account-form">
+        <n-form
+          ref="formRef"
+          label-placement="left"
+          size="large"
+          :model="formInline"
+          :rules="rules"
+        >
+          <n-form-item path="username">
+            <n-input v-model:value="formInline.username" placeholder="请输入用户名">
+              <template #prefix>
+                <n-icon size="18" color="#808695">
+                  <PersonOutline />
+                </n-icon>
+              </template>
+            </n-input>
+          </n-form-item>
+          <n-form-item path="password">
+            <n-input
+              v-model:value="formInline.password"
+              type="password"
+              showPasswordOn="click"
+              placeholder="请输入密码"
+            >
+              <template #prefix>
+                <n-icon size="18" color="#808695">
+                  <LockClosedOutline />
+                </n-icon>
+              </template>
+            </n-input>
+          </n-form-item>
+          <n-form-item class="default-color">
+            <div class="flex justify-between">
+              <div class="flex-initial">
+                <n-checkbox v-model:checked="autoLogin">自动登录</n-checkbox>
+              </div>
+              <div class="flex-initial order-last">
+                <a href="javascript:">忘记密码</a>
+              </div>
+            </div>
+          </n-form-item>
+          <n-form-item>
+            <n-button type="primary" @click="handleSubmit" size="large" :loading="loading" block>
+              登录
+            </n-button>
+          </n-form-item>
+          <n-form-item class="default-color">
+            <div class="flex view-account-other">
+              <div class="flex-initial">
+                <span>其它登录方式</span>
+              </div>
+              <div class="flex-initial mx-2">
+                <a href="javascript:">
+                  <n-icon size="24" color="#2d8cf0">
+                    <LogoGithub />
+                  </n-icon>
+                </a>
+              </div>
+              <div class="flex-initial mx-2">
+                <a href="javascript:">
+                  <n-icon size="24" color="#2d8cf0">
+                    <LogoFacebook />
+                  </n-icon>
+                </a>
+              </div>
+              <div class="flex-initial" style="margin-left: auto">
+                <a href="javascript:">注册账号</a>
+              </div>
+            </div>
+          </n-form-item>
+        </n-form>
+      </div>
+    </div>
+  </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { reactive, ref } from 'vue';
+  import { useMessage } from 'naive-ui'
+  import { PersonOutline, LockClosedOutline, LogoGithub, LogoFacebook } from '@vicons/ionicons5';
+  import { websiteConfig } from '@/config/website.config';
+  const formRef = ref();
+  const message = useMessage();
+  const loading = ref(false);
+  const autoLogin = ref(true);
+
+  const formInline = reactive({
+    username: 'admin',
+    password: '123456',
+    isCaptcha: true,
+  });
+  const rules = {
+    username: { required: true, message: '请输入用户名', trigger: 'blur' },
+    password: { required: true, message: '请输入密码', trigger: 'blur' },
+  };
+  const handleSubmit = () => {}
+</script>
+
+<style scoped>
+  .view-account {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    overflow: auto;
+
+    &-container {
+      flex: 1;
+      padding: 32px 12px;
+      max-width: 384px;
+      min-width: 320px;
+      margin: 0 auto;
+    }
+
+    &-top {
+      padding: 32px 0;
+      text-align: center;
+
+      &-desc {
+        font-size: 14px;
+        color: #808695;
+      }
+    }
+
+    &-other {
+      width: 100%;
+    }
+
+    .default-color {
+      color: #515a6e;
+
+      .ant-checkbox-wrapper {
+        color: #515a6e;
+      }
+    }
+  }
+
+  @media (min-width: 768px) {
+    .view-account {
+      background-image: url('../../assets/images/login.svg');
+      background-repeat: no-repeat;
+      background-position: 50%;
+      background-size: 100%;
+    }
+
+    .page-account-container {
+      padding: 32px 0 24px 0;
+    }
+  }
+</style>
