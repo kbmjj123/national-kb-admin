e60febf0815057c5e3aa6b59886ea0ea305eaf44
郑耿林 <kbmjj123@gmail.com>
2024-02-21 05:40:52 +0800

    📃 docs(typescript): 补充关于全局变量/类型的定义说明

--- types/window.d.ts
+++ types/window.d.ts
@@ -1,8 +1,8 @@
 declare global {
   interface Window {
-    $message: import('naive-ui').MessageApi;
-    $dialog: import('naive-ui').DialogApi;
-    $notification: import('naive-ui').NotificationApi;
-    $loadingBar: import('naive-ui').LoadingBarApi;
+    $message: import('naive-ui').MessageApi
+    $dialog: import('naive-ui').DialogApi
+    $notification: import('naive-ui').NotificationApi
+    $loadingBar: import('naive-ui').LoadingBarApi
   }
 }
