53e4334510794907e9bc5f1c16b71983c26c745c
郑耿林 <kbmjj123@gmail.com>
2024-03-22 06:39:14 +0800

    ✨ feat(统一loadingbar): 在页面顶部加上统一的loadingbar

--- src/router/guards.ts
+++ src/router/guards.ts
@@ -2,8 +2,12 @@ import { Router } from 'vue-router'
 import { ACCESS_TOKEN } from '../store/mutation-types'
 // 创建全局路由守卫
 export function createRouterGuards(router: Router) {
-  // router.beforeEach((to, from ,next) => {
-  // })
-  // router.afterEach((to, from, failure) => {
-  // })
+  router.beforeEach((to, from ,next) => {
+		console.info(window['$loadingBar'])
+		window['$loadingBar'] && window['$loadingBar'].start()
+		next()
+  })
+  router.afterEach((to, from, failure) => {
+		window['$loadingBar'] && window['$loadingBar'].finish()
+  })
 }
