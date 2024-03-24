ae7bd19717aed865ad4456e9ce96462f8771793f
郑耿林 <kbmjj123@gmail.com>
2024-03-25 05:37:05 +0800

    ✨ feat(统一的重定向): 当页面未找到时候，统一重定向到根路径

--- src/router/constantRouter.ts
+++ src/router/constantRouter.ts
@@ -18,19 +18,24 @@ export const constantRouter: RouteRecordRaw[] = [
     },
   },
 	{
-		path: '/',
-		name: '/404',
+		path: '/404',
+		name: '404',
 		component: () => import('@/views/404/index.vue'),
 		meta: {
 			title: '页面没找到'
 		}
 	},
 	{
-		name: '/',
 		path: '/403',
+		name: '403',
 		component: () => import('@/views/403/index.vue'),
 		meta: {
 			title: '没有权限'
 		}
+	}, 
+	{
+		path: '*',
+		name: '*',
+		redirect: '/'
 	}
 ]
