d303d7512abca2688a19b561f11bf46e480aa5cb
郑耿林 <kbmjj123@gmail.com>
2024-04-26 06:50:25 +0800

    ✨ feat(品牌界面开发): 新增品牌界面功能

---
+++ src/views/brand/component/add-brand-modal.vue
@@ -0,0 +1,41 @@
+<template>
+	<n-button type="primary" @click="onShowModal">新增</n-button>
+	<n-modal v-model:show="showModal"
+		preset="dialog"
+		title="编辑品牌"
+		@negative-click="onHideModal"
+		@positive-click="onAddOrEditAction"
+	>
+		<n-form inline ref="editBrandForm" :model="brandForm" :rules="brandFormRules">
+			<n-form-item label="品牌名称: ">
+				<n-input v-model:value="brandForm.brandName" placeholder="请输入品牌名称"></n-input>
+			</n-form-item>
+		</n-form>
+	</n-modal>
+</template>
+
+<script setup lang="ts">
+	import { ref, reactive } from 'vue'
+	const showModal = ref(false)
+	const onShowModal = () => {
+		showModal.value = true
+	}
+	const onHideModal = () => {
+		showModal.value = false
+	}
+	const brandForm = reactive({
+		brandName: ''
+	})
+	const editBrandForm = ref()
+	const brandFormRules = {
+		brandName: {
+			required: true,
+			message: '请输入品牌名称',
+			trigger: 'blur'
+		}
+	}
+	// 新增或者编辑操作
+	const onAddOrEditAction = () => {
+
+	}
+</script>
--- src/views/brand/index.vue
+++ src/views/brand/index.vue
@@ -1,5 +1,71 @@
 <template>
-	<div>我是品牌管理</div>
+	<n-form
+		ref="brandFormRef"
+		inline
+		:model="brandForm"
+		:label-width="80">
+		<n-form-item label="品牌关键词" path="key">
+			<n-input v-model:value="brandForm.key" placeholder="请输入品牌关键词" clearable></n-input>
+		</n-form-item>
+		<n-form-item :label-width="0">
+			<n-button type="primary" @click="getBrandList">搜索</n-button>
+			<n-button>重置</n-button>
+		</n-form-item>
+	</n-form>
+	<n-data-table
+		bordered
+		bottom-bordered
+		:columns="columns"
+		:data="brandList"
+		:loading="loading"
+		:pagination="pagination"
+	></n-data-table>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import { ref, reactive, onMounted, Ref } from 'vue'
+	import type { DataTableColumns } from 'naive-ui'
+
+	const brandForm = reactive({
+		key: ''
+	})
+
+	const columns: Ref<DataTableColumns> = ref([
+		{
+			title: '',
+			key: ''
+		}
+	])
+
+	const brandList = ref([])
+
+	const loading = ref(false)
+	const params = reactive({
+		pageIndex: 0,
+		pageSize: 20,
+		key: ''
+	})
+	const pagination = reactive({
+		page: params.pageIndex,
+		pageSize: params.pageSize,
+		showSizePicker: true,
+		onChange: (page: number) => {
+			pagination.page = page
+			getBrandList()
+		},
+		onUpdatePageSize: (pageSize: number) => {
+			pagination.pageSize = pageSize
+			pagination.page = 1
+			getBrandList()
+		}
+	})
+	// 获取分类列表数据
+	const getBrandList = () => {
+		
+	}
+
+	onMounted(() => {
+		getBrandList()
+	})
+
+</script>
