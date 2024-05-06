34b7b8efc5ba67a25ecabd5733313f22c5606b72
郑耿林 <kbmjj123@gmail.com>
2024-05-06 13:45:42 +0800

    ✨ feat(category): 新增代码格式化

--- src/views/category/component/CateItem.vue
+++ src/views/category/component/CateItem.vue
@@ -1,12 +1,12 @@
 <template>
-	<div class="p-12 bg-white border-b">
-		{{ itemInfo.name }}
-		<n-space>
-			<n-button text type="primary" @click="onAddCate">新增</n-button>
-			<n-button text type="primary" @click="onEditCate">编辑</n-button>
-			<n-button text type="error" @click="onDeleteCate">删除</n-button>
-		</n-space>
-	</div>
+  <div class="p-12 bg-white border-b">
+    {{ itemInfo.name }}
+    <n-space>
+      <n-button text type="primary" @click="onAddCate">新增</n-button>
+      <n-button text type="primary" @click="onEditCate">编辑</n-button>
+      <n-button text type="error" @click="onDeleteCate">删除</n-button>
+    </n-space>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -15,33 +15,32 @@ import { useDialog } from 'naive-ui'
 const dialog = useDialog()
 
 const { itemInfo } = defineProps<{
-	itemInfo: CateType
+  itemInfo: CateType
 }>()
 
 const emit = defineEmits<{
-	'on-add-cate': [],
-	'on-edit-cate': [cateInfo: CateType],
-	'on-delete-cate': [cateInfo: CateType]
+  'on-add-cate': []
+  'on-edit-cate': [cateInfo: CateType]
+  'on-delete-cate': [cateInfo: CateType]
 }>()
 
 const onAddCate = () => {
-	emit('on-add-cate')
+  emit('on-add-cate')
 }
 
-const onEditCate =() => {
-	emit('on-edit-cate', itemInfo)
+const onEditCate = () => {
+  emit('on-edit-cate', itemInfo)
 }
 
 const onDeleteCate = () => {
-	dialog.create({
-		title: `温馨提示`,
-		content: `您确定要删除分类：${itemInfo.name}吗？`,
-		negativeText: '我再想想',
-		positiveText: '确定',
-		onPositiveClick: () => {
-			emit('on-delete-cate', itemInfo)
-		}
-	})
+  dialog.create({
+    title: `温馨提示`,
+    content: `您确定要删除分类：${itemInfo.name}吗？`,
+    negativeText: '我再想想',
+    positiveText: '确定',
+    onPositiveClick: () => {
+      emit('on-delete-cate', itemInfo)
+    },
+  })
 }
-
 </script>
--- src/views/category/component/EditCateModal.vue
+++ src/views/category/component/EditCateModal.vue
@@ -1,16 +1,23 @@
 <template>
-	<n-modal
-		v-model:show="model"
-		preset="dialog"
-		title="编辑分类"
-		negative-text="取消"
-		positive-text="确定"
-		@positive-click="onAddOrEditAction"
-	>
-		<n-form inline label-placement="left" label-width="auto" ref="editCateForm" :model="cateForm" :rules="cateFormRules">
-			<n-form-item></n-form-item>
-		</n-form>
-	</n-modal>
+  <n-modal
+    v-model:show="model"
+    preset="dialog"
+    title="编辑分类"
+    negative-text="取消"
+    positive-text="确定"
+    @positive-click="onAddOrEditAction">
+    <n-form
+      inline
+      label-placement="left"
+      label-width="auto"
+      ref="editCateForm"
+      :model="cateForm"
+      :rules="cateFormRules">
+      <n-form-item label="上级分类:">
+				<n-select clearable placeholder="请选择分类"></n-select>
+			</n-form-item>
+    </n-form>
+  </n-modal>
 </template>
 
 <script setup lang="ts">
@@ -18,55 +25,54 @@ import { ref, reactive, watch } from 'vue'
 import { CateType, addCate, editCate } from '@/api/product/category'
 
 const { itemInfo } = defineProps<{
-	itemInfo: CateType
+  itemInfo: CateType
 }>()
 
 const emit = defineEmits<{
-	'on-success': [cate: CateType]
+  'on-success': [cate: CateType]
 }>()
 
 const model = defineModel({
-	type: Boolean
+  type: Boolean,
 })
 const cateFormRules = {
-	name: {
-		required: true,
-		message: '请输入分类名称',
-		trigger: 'blur'
-	}
+  name: {
+    required: true,
+    message: '请输入分类名称',
+    trigger: 'blur',
+  },
 }
 
 watch(model, (newVal) => {
-	if(newVal){
-		cateForm.id = itemInfo.id
-		cateForm.name = itemInfo.name
-	}
+  if (newVal) {
+    cateForm.id = itemInfo.id
+    cateForm.name = itemInfo.name
+  }
 })
 
 const editCateForm = ref()
 const cateForm = reactive<CateType>({
-	id: '',
-	name: ''
+  id: '',
+  name: '',
 })
 
 // 新增或者编辑操作
 const onAddOrEditAction = () => {
-	return new Promise(resolve => {
-		editCateForm.value?.validate(async (errors) => {
-			if(!errors){
-				let res: any
-				if(cateForm.id){
-					res = await editCate(cateForm)
-				}else{
-					res = await addCate(cateForm)
-				}
-				emit('on-success', res?.data)
-				resolve(true)
-			}else{
-				resolve(false)
-			}
-		})
-	})
+  return new Promise((resolve) => {
+    editCateForm.value?.validate(async (errors) => {
+      if (!errors) {
+        let res: any
+        if (cateForm.id) {
+          res = await editCate(cateForm)
+        } else {
+          res = await addCate(cateForm)
+        }
+        emit('on-success', res?.data)
+        resolve(true)
+      } else {
+        resolve(false)
+      }
+    })
+  })
 }
-
 </script>
