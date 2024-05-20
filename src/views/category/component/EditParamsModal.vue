411322e33ed028a6d533926d43f9a34742a51ec3
郑耿林 <kbmjj123@gmail.com>
2024-05-20 23:27:12 +0800

    ✨ feat(category): 新增编辑商品属性的对话框

---
+++ src/views/category/component/EditParamsModal.vue
@@ -0,0 +1,14 @@
+<template>
+	<n-modal v-model:show="model" preset="card" title="编辑商品属性">
+		<n-form>
+			<n-form-item></n-form-item>
+		</n-form>
+	</n-modal>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const model = defineModel()
+
+</script>
